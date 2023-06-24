3f8842d547c559c256d98bd0eaa5461ce2bc94ba
4cha <kgchan07@naver.com>
2023-06-24 21:05:23 +0900

    message

---
+++ lib/screens/bleeper_screen_01.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BleePer_Group extends StatefulWidget {
+  const BleePer_Group({super.key});
+
+  @override
+  State<BleePer_Group> createState() => _BleePer_GroupState();
+}
+
+class _BleePer_GroupState extends State<BleePer_Group> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Bleeper_Widjet extends StatelessWidget {
+  const Bleeper_Widjet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
